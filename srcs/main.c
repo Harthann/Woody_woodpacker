0498838609ca84ee97d13e471d270495247cc2e6
Nicolas EYRAUD <nieyraud@e2r5p10.clusters.42paris.fr>
2022-02-23 18:07:23 +0100

    adding loading file function

--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <woody.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-int main(void)
+char *load_file(char *path)
 {
-    printf("Hello World\n");
-    _payload();
-    printf("%ld\n", _falseend - _payload);
+	int fd, len;
+	char *file;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return NULL;
+	len = lseek(fd, 0, SEEK_END);
+	printf("File loaded length: %d\n", len);
+	lseek(fd, 0, SEEK_SET);
+	file = mmap(NULL, len + 1, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);	
+	close(fd);
+	return file;
+}
+
+int main(int ac, char **av)
+{
+	char *file = NULL;
+	if (ac != 2)
+	{
+		printf("Be carefull error\n");
+	}
+	printf("Hello World\n");
+	_payload();
+	printf("%ld\n", _falseend - _payload);
+	file = load_file(av[1]);
+	if (!file)
+	{
+		write(2, "Failed to load file\n",20);
+		return 0;
+	}
+	printf("%s\n", file);
 }
 
